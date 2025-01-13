66f1fb9f47cc60708d4db36de42e74a277474723
petertasca <tascapeter@gmail.com>
2025-01-13 16:59:37 -0500

    header started - h1 and icon added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-
 import './App.css'
+import Openbook from './assets/open-book.png'
 
 function App() {
   
 
   return (
-    <h1>Hello World</h1>
+    <div className="login-container">
+      <header className="login-header">
+        <img src={Openbook} alt="open-book" className="open-book" />
+        <h1>Bookchat</h1>
+      </header>
+      <main className="login-main">
+
+      </main>
+    </div>
+    
     
   )
 }
