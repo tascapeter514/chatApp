b82889403ebb521b2692a2652a058197a1e2153e
petertasca <tascapeter@gmail.com>
2025-01-14 17:47:43 -0500

    new start with vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,38 @@
+
+
 import './App.css'
-import Openbook from './assets/open-book.png'
 
 function App() {
-  
+
 
   return (
     <div className="login-container">
       <header className="login-header">
-        <img src={Openbook} alt="open-book" className="open-book" />
+        <img src="" alt="open-book" />
         <h1>Bookchat</h1>
       </header>
       <main className="login-main">
-        <form action="" className="">
+        <form action="" className="login-form">
           <div className="form-control">
-            <label htmlFor="" className="username">
-              <input type="text" />
-            </label>
+            <label htmlFor="" className="username">UserName</label>
+            <input name='username' id='username' placeholder='Enter username...' type="text" className="username-input" required/>
           </div>
+          <div className="form-control">
+            <label htmlFor="" className="room">Room</label>
+            <select name="" id="" className="room">
+              <option value="Shakespeare">Shakespeare</option>
+              <option value="Kafka">Kafka</option>
+              <option value="Dickens">Dickens</option>
+            </select>
+          </div>
+          <button type='submit' className='join-btn'></button>
+
         </form>
-        <div className="form-control">
-          <label htmlFor="" className="room">
-            <input type="text" />
-          </label>
-        </div>
-
-
       </main>
+      
+      
     </div>
-    
-    
+        
   )
 }
 
